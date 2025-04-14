54a7ad9beb33e81aefce7a8ff44f87975a793642
Marci <marcikill3@gmail.com>
2025-04-14 18:44:18 +0200

    Navbar created

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import ProductCard from './components/ProductCard';
+//import ProductCard from './components/ProductCard';
 //import Cart from './pages/Cart';
 //import ProductDetails from './pages/ProductDetails';
-import Navbar from './components/Navbar';
+//import Navbar from './components/Navbar';
 
 function App() {
   return (
     <>
-    <Navbar>
+  
       <Routes>
       <Route path="/" element={<Home />} />
-      <Route path='/product' element={<ProductCard />} />
+      
       </Routes>
-      </Navbar>
+      
     </>
   );
 }
